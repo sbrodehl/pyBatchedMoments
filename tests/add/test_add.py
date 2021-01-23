3e18322ff00d309d70d4ce1ac6798ab2dca2963c
Sebastian Brodehl <brodehl@uni-mainz.de>
2021-01-23 22:11:06 +0100

    Add batched add test.

--- tests/add/test_add.py
+++ tests/add/test_add.py
@@ -14,3 +14,16 @@ def test_iadd():
     b = BatchedMoments()(data[:len(data) // 2])
     b += BatchedMoments()(data[len(data) // 2:])
     assert a == b
+
+
+def test_batched_add():
+    data = list(range(100))
+    batchsize = 10
+    batched = BatchedMoments()(data[:batchsize])
+    for idx in range(1, len(data) // batchsize):
+        st = idx * batchsize
+        batched += BatchedMoments()(data[st: st + batchsize])
+        partial = BatchedMoments()(data[: st + batchsize])
+        assert partial == batched
+    full = BatchedMoments()(data)
+    assert full == batched
