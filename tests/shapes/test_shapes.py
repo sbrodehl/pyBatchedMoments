86a413e71aac7c775207d159b542751586b10964
Sebastian Brodehl <brodehl@uni-mainz.de>
2021-01-23 21:48:17 +0100

    Fix error in assert. Use `shape` property.

--- tests/shapes/test_shapes.py
+++ tests/shapes/test_shapes.py
@@ -6,18 +6,19 @@ from batchedmoments import BatchedMoments
 def test_shape_None():
     bm = BatchedMoments(shape=None)
     assert bm._initialized is False  # first update call will initialize
-    assert bm._moments_shape is None  # moments shape is zero
+    assert bm.shape is None  # moments shape is zero
 
 
 def test_shape_0d():
     shp = tuple()
     bm = BatchedMoments(shape=shp)
     assert bm._initialized is True
-    assert bm._moments_shape == shp
+    assert bm.shape == shp
 
 
 def test_shape_random_nd():
     shp = tuple([4] * randint(1, 16))
     bm = BatchedMoments(shape=shp)
     assert bm._initialized is True
-    assert bm._moments_shape == shp
+    # axis=None, thus moments shape is `()`
+    assert bm.shape == tuple()
