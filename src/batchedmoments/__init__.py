5925d737a77d6199b162b5b1760fd3edc6083c95
Sebastian Brodehl <brodehl@uni-mainz.de>
2021-01-23 23:46:50 +0100

    Initial release 0.0.1

---
+++ src/batchedmoments/__init__.py
@@ -0,0 +1,21 @@
+# pylint: disable=unsubscriptable-object
+from typing import Union, Iterable
+import warnings
+import numpy as np
+
+__version__       = "0.0.1"
+__title__         = "batchedmoments"
+__description__   = "Computes (batch-wise) sample statistics."
+__url__           = "https://github.com/sbrodehl/PyBatchedMoments"
+__uri__           = __url__
+__doc__           = __description__ + " <" + __uri__ + ">"
+__documentation__ = __url__
+__source__        = __url__
+__tracker__       = __url__ + '/issues'
+__author__        = "Sebastian Brodehl"
+__license__       = "MIT License"
+__copyright__     = "Copyright (c) 2021 " + __author__
+
+
+class BatchedMoments:
+    pass
