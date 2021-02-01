7d694a9c963fd78522381b59e3db192fb4a55261
Sebastian Brodehl <brodehl@uni-mainz.de>
2021-02-01 14:21:51 +0100

    Add assert with sequential method.

--- tests/multiprocessing/test_multiprocessing.py
+++ tests/multiprocessing/test_multiprocessing.py
@@ -1,5 +1,6 @@
 import multiprocessing
 from multiprocessing import Pool
+from itertools import tee
 
 from batchedmoments import BatchedMoments
 
@@ -7,8 +8,14 @@ from batchedmoments import BatchedMoments
 def test_multiprocessing_add():
     batchsize = 15
     samples = batchsize * batchsize * batchsize * batchsize
-    data = iter(list(range(n, n + batchsize)) for n in range(0, samples, batchsize))
+    gen1, gen2 = tee((list(range(n, n + batchsize)) for n in range(0, samples, batchsize)))
+    data = iter(gen1)
     bm = BatchedMoments()(next(data))
     with Pool(processes=multiprocessing.cpu_count()) as pool:
         for dbm in pool.imap_unordered(BatchedMoments(), data):
             bm += dbm
+    data = iter(gen2)
+    seq = BatchedMoments()(next(data))
+    for batch in data:
+        seq += BatchedMoments()(batch)
+    assert seq == bm
